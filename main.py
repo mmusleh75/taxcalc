949e9557c727bf239ec644c4d2329e49fe62b8b6
Ashraf <ashraf.musleh02@gmail.com>
2021-09-18 17:07:32 -0500

    Comment

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ SingleBracket2 = 30000
 MarriedJointlyBracket1 = 30000
 MarriedJointlyBracket2 = 60000
 
+MarriedSeparatelyBracket1 = 15000
+MarriedSeparatelyBracket2 = 30000
+
+HeadofHouseholdBracket1 = 15000
+HeadofHouseholdBracket2 = 30000
 def SingleTaxCalculator(gSalary):
 
     Tax = 0
@@ -21,8 +26,10 @@ def SingleTaxCalculator(gSalary):
         Tax = (SingleBracket1 * 3.1) / 100
         Tax = Tax + (gSalary * 5.25) / 100
     else:
-        # TODO 1
-        Tax = 0
+        gSalary = gSalary - SingleBracket1 - SingleBracket2
+        Tax = (SingleBracket1 * 3.1) / 100
+        Tax = Tax + (SingleBracket2 * 5.25) / 100
+        Tax = Tax + (gSalary * 5.7) / 100
 
     return Tax, OriginalSalary - Tax
 
@@ -39,22 +46,52 @@ def MarriedJointlyTaxCalculator(gSalary):
         Tax = Tax + (gSalary * 5.25) / 100
     else:
         # TODO 2
-        Tax = 0
+        gSalary = gSalary - MarriedJointlyBracket1 - MarriedJointlyBracket2
+        Tax = (MarriedJointlyBracket1 * 3.1) / 100
+        Tax = Tax + (MarriedJointlyBracket2 * 5.25) / 100
+        Tax = Tax + (gSalary * 5.7) / 100
 
     return Tax, OriginalSalary - Tax
 
 def MarriedSeparatelyTaxCalculator(gSalary):
-    # TODO 3
-    return 0, 0
+    Tax = 0
+    OriginalSalary = gSalary
+
+    if gSalary <= MarriedSeparatelyBracket1:
+        Tax = (gSalary * 3.1) / 100
+    elif (gSalary > MarriedSeparatelyBracket1) & (gSalary <= MarriedSeparatelyBracket2):
+        gSalary = gSalary - MarriedSeparatelyBracket1
+        Tax = (MarriedSeparatelyBracket1 * 3.1) / 100
+        Tax = Tax + (gSalary * 5.25) / 100
+    else:
+        # TODO 3
+        gSalary = gSalary - MarriedSeparatelyBracket1 - MarriedSeparatelyBracket2
+        Tax = (MarriedSeparatelyBracket1 * 3.1) / 100
+        Tax = Tax + (MarriedSeparatelyBracket2 * 5.25) / 100
+        Tax = Tax + (gSalary * 5.7) / 100
+    return Tax, OriginalSalary - Tax
 
 def HeadOfHouseholdTaxCalculator(gSalary):
-    # TODO 4
-    return 0, 0
+    Tax = 0
+    OriginalSalary = gSalary
+
+    if gSalary <= HeadofHouseholdBracket1:
+        Tax = (gSalary * 3.1) / 100
+    elif (gSalary > HeadofHouseholdBracket1) & (gSalary <= HeadofHouseholdBracket2):
+        gSalary = gSalary - HeadofHouseholdBracket1
+        Tax = (HeadofHouseholdBracket1 * 3.1) / 100
+        Tax = Tax + (gSalary * 5.25) / 100
+    else:
+        gSalary = gSalary - HeadofHouseholdBracket1 - HeadofHouseholdBracket2
+        Tax = (HeadofHouseholdBracket1 * 3.1) / 100
+        Tax = Tax + (HeadofHouseholdBracket2 * 5.25) / 100
+        Tax = Tax + (gSalary * 5.7) / 100
+    return Tax, OriginalSalary - Tax
 
 if __name__ == '__main__':
 
     vName = "Steve Doe"
-    vSalary = 100000
+    vSalary = 50000
 
     Tax = SingleTaxCalculator(vSalary)
     # Tax[0] is the tax to be paid
